9bf127658b3bb496b21fbe18b273d655b855e3b7
Nixon Patino <nixonp@gmail.com>
2012-11-17 09:40:23 -0530

    Update src/pruebaclases/PruebaClases.java

--- src/pruebaclases/PruebaClases.java
+++ src/pruebaclases/PruebaClases.java
@@ -15,5 +15,6 @@ public class PruebaClases {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        // hola esto es para probar la edicion..
     }
 }
